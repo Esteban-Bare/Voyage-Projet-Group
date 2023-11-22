cfee8d61c9e725ddbe46162f654ea0b1e3e742fc
LordDragons <149957558+LordDragons@users.noreply.github.com>
2023-11-22 10:11:10 +0100

    desccrip ecosse
    changement de descriptif sur ecosse.html

--- voyage.js
+++ voyage.js
@@ -33,5 +33,5 @@ function showSlides() {
         slideIndex = 1;
     }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
